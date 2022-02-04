1937a2a5a3ce893ef5277c697fa5303926afda91
Matthew Boyd <Matthew.boyd@deliveroo.co.uk>
2022-02-04 20:20:36 +0000

    removing redis unused code and updating to sqlx

--- activities.go
+++ activities.go
@@ -2,11 +2,11 @@ package activities
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
-	_ "github.com/lib/pq" //for sql
+	_ "github.com/jackc/pgx" //for sql
+	"github.com/jmoiron/sqlx"
 	"github.com/sony/gobreaker"
 	"io/ioutil"
 	"log"
@@ -24,7 +24,7 @@ type Activities struct {
 
 type Handler struct {
 	Logger         log.Logger
-	Db             sql.DB
+	Db             sqlx.DB
 	Redis          redis.Client
 	CircuitBreaker *gobreaker.CircuitBreaker
 }
@@ -74,24 +74,6 @@ type Weather struct {
 	Cod      int    `json:"cod"`
 }
 
-var ActivitiesList = []Activities{
-	{"shopping belfast", "Belfast", true},
-	{"mini golf", "Craigavon", true},
-	{"shopping dublin", "Dublin", true},
-	{"cinema", "Lisburn", false},
-	{"axe throwing", "Belfast", false},
-	{"swimming", "Armagh", false},
-	{"Walk around Stormount", "Belfast", false},
-	{"Hike Cavehill", "Belfast", true},
-	{"Trip to the causeway coast", "Coleraine", true},
-	{"Walk around castlewellan", "Castlewellan", true},
-	{"Gosford Forest Park", "Armagh", true},
-	{"Go to the Ulster folk and Transport Museum", "Omagh", false},
-	{"Slieve Guillion Trip", "Armagh", true},
-	{"Marble Arch Caves", "Fermanagh", true},
-	{"Cuilcagh (Stairway to Heaven)", "Fermanagh", true},
-}
-
 func (h *Handler) SunnyEndpoint() func(writer http.ResponseWriter, request *http.Request) {
 	//h.db.QueryContext()
 	return func(writer http.ResponseWriter, request *http.Request) {
@@ -106,23 +88,14 @@ func (h *Handler) SunnyEndpoint() func(writer http.ResponseWriter, request *http
 }
 
 func (h *Handler) getSunnyActivity(ctx context.Context) string {
-	var a Activities
-	var newActivityList []Activities
+	var activityList []Activities
 
-	sunnyActivitiesQuery := "SELECT * FROM activities where sunny = $1"
-	rows, err := h.Db.QueryContext(ctx, sunnyActivitiesQuery, true)
+	err := h.Db.Select(&activityList, "SELECT * FROM activities where sunny = true")
 	if err != nil {
 		log.Fatalln("An error occurred", err)
 	}
-	for rows.Next() {
-		err = rows.Scan(&a.Name, &a.Postcode, &a.Sunny)
-		if err != nil {
-			log.Fatalln("Error in scanning db rows", err)
-		}
-		newActivityList = append(newActivityList, a)
-	}
 	var discardedActivityList []Activities
-	choosenActivity, _ := h.retrieveActivity(ctx, newActivityList, discardedActivityList, true)
+	choosenActivity, _ := h.retrieveActivity(ctx, activityList, discardedActivityList, true)
 	return fmt.Sprintf("%s %s", choosenActivity.Name, choosenActivity.Postcode)
 }
 
@@ -214,38 +187,6 @@ func (h *Handler) getNotSunnyActivities(ctx context.Context) string {
 	var discardedActivityList []Activities
 	choosenActivity, _ := h.retrieveActivity(ctx, newActivityList, discardedActivityList, false)
 	return fmt.Sprintf("%s %s", choosenActivity.Name, choosenActivity.Postcode)
-
-	//value, err := h.Redis.Get(ctx, "allWeatherActivities").Result()
-	//if err == redis.Nil {
-	//	var a Activities
-	//	var newActivityList []Activities
-	//
-	//	notSunnyActivitiesQuery := "SELECT * FROM activities where sunny = $1"
-	//	rows, err := h.Db.Query(notSunnyActivitiesQuery, false)
-	//	if err != nil {
-	//		log.Fatalln("An error occurred", err)
-	//	}
-	//	for rows.Next() {
-	//		err = rows.Scan(&a.Name, &a.Postcode, &a.Sunny)
-	//		if err != nil {
-	//			log.Fatalln("Error in scanning db rows", err)
-	//		}
-	//		newActivityList = append(newActivityList, a)
-	//	}
-	//
-	//	h.Redis.Set(ctx, "allWeatherActivites", newActivityList, time.Minute*15)
-	//	var discardedActivityList []Activities
-	//	choosenActivity, _ := h.retrieveActivity(ctx, newActivityList, discardedActivityList, false)
-	//	return fmt.Sprintf("%s %s", choosenActivity.Name, choosenActivity.Postcode), nil
-	//} else if err != nil {
-	//	return "error", err
-	//} else {
-	//	// build response
-	//	var discardedActivityList []Activities
-	//	choosenActivity, _ := h.retrieveActivity(ctx, value, discardedActivityList, false)
-	//	return fmt.Sprintf("%s %s", choosenActivity.Name, choosenActivity.Postcode), nil
-	//	return value, nil
-	//}
 }
 
 func (h *Handler) RemoveIndex(s []Activities, index int) []Activities {
--- go.mod
+++ go.mod
@@ -4,11 +4,21 @@ go 1.17
 
 require (
 	github.com/go-redis/redis/v8 v8.11.4
-	github.com/lib/pq v1.10.4
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jmoiron/sqlx v1.3.4
 	github.com/sony/gobreaker v0.5.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
